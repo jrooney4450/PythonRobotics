43ece4b8ab02fda64bb3fb5b794be5e9337421bf
Justin Rooneygit config --global user.name Justin <justin@dexai.com>
2019-12-09 11:23:29 -0500

    feat: accurate state
    correctly implemented station for gain comparisons

--- PathTracking/stanley_controller/stanley_controller.py
+++ PathTracking/stanley_controller/stanley_controller.py
@@ -67,7 +67,7 @@ def pid_control(target, current):
     return Kp * (target - current)
 
 
-def stanley_control(state, cx, cy, cyaw, idx_now):
+def stanley_control(state, cx, cy, cyaw, idx_now, sta_before):
     """
     Stanley steering control.
 
@@ -79,7 +79,7 @@ def stanley_control(state, cx, cy, cyaw, idx_now):
     :return: (float, int)
     """
     # idx_next, e_ct = calc_target_index(state, cx, cy)
-    idx_next, e_ct, sta = calc_next_index(state, cx, cy, idx_now)
+    idx_next, e_ct, sta, sta_before = calc_next_index(state, cx, cy, idx_now, sta_before)
 
     # if idx_now >= idx_next:
     #     idx_next = idx_now
@@ -91,7 +91,7 @@ def stanley_control(state, cx, cy, cyaw, idx_now):
     # Steering control
     delta = theta_e + theta_d
 
-    return delta, idx_next, e_ct, sta
+    return delta, idx_next, e_ct, sta, sta_before
 
 
 def normalize_angle(angle):
@@ -136,14 +136,14 @@ def calc_target_index(state, cx, cy):
 
     return idx_now, e_ct
 
-def calc_next_index(state, cx, cy, idx_now):
+def calc_next_index(state, cx, cy, idx_now, sta_before):
     """
     Compute next index in the trajectory.
 
     :param state: (State object)
     :param cx: [float]
     :param cy: [float]
-    :return: next_idx, ect (int, float)
+    :return: next_idx, e_ct (cross-track error), sta (station) [int, float, float]
     """
     # Calc front axle position
     fx = state.x + L * np.cos(state.yaw)
@@ -153,38 +153,50 @@ def calc_next_index(state, cx, cy, idx_now):
     r_T = np.array([cx[idx_now], cy[idx_now]]) # desired position vector
     r = np.array([fx, fy])
 
-    sta_vect = np.array([cx[idx_next] - cx[idx_now], cy[idx_next] - cy[idx_now]]) # station vector
+    sta_vect = np.array([cx[idx_next] - cx[idx_now], cy[idx_next] - cy[idx_now]]) # station vector, or vector from current point on path to next point on path
     sta = np.linalg.norm(sta_vect) # magnitude of the station vector
-    a = sta_vect / np.linalg.norm(sta) # tangent unit vector
+    a = sta_vect / np.linalg.norm(sta) # trajectory tangent unit vector
+
+    # sta_before = sta
+    # print('The station before is: {}'.format(sta_before))
+    sta = np.dot((r - r_T), a)
+    # print('The station is: {}'.format(sta))
+    sta_inc = sta - sta_before # station is actually the projection of the position onto the tangent vector minus the previous amount
+    if sta_inc < 0:
+        sta_inc += ds
+    # print('The station increment is: {}'.format(sta_inc))
+    sta_before = sta
+
     normal = np.empty_like(a)
     normal[0] = -a[1]
     normal[1] = a[0]
     normal = normal / np.linalg.norm(normal)
-    print('The tangent vector is: {}'.format(a))
-    print('The normal vector is: {}'.format(normal))
+    # print('The tangent vector is: {}'.format(a))
+    # print('The normal vector is: {}'.format(normal))
 
     # Eq. 9 in the following paper:
     # http://ai.stanford.edu/~gabeh/papers/GNC08_QuadTraj.pdf
-    e_ct = np.matmul((r_T - r), normal) # cross-track position error
+    e_ct = np.dot((r_T - r), normal) # cross-track position error
 
     b = np.array([fx - cx[idx_next], fy - cy[idx_next]]) # vector from next index point to current robot state
-    print('The next index to state vector is: {}'.format(b))
+    # print('The next index to state vector is: {}'.format(b))
 
     angle = np.arccos(np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b)))
 
-    print('The angle is: {}'.format(angle))
+    # print('The angle is: {}'.format(angle))
     if angle < (np.pi/2):
-        print("incremented the counter")
+        # print("incremented the counter")
         idx_now += 1
 
-    print('idx_next: {}'.format(idx_next))
-    print('cross-track error: {}'.format(e_ct))
+    # print('idx_next: {}'.format(idx_next))
+    # print('cross-track error: {}'.format(e_ct))
 
-    return idx_now, e_ct, sta
+    return idx_now, e_ct, sta_inc, sta_before
 
 
 def main():
     """Plot an example of Stanley steering control on a cubic spline."""
+    print("Starting main loop with k={}".format(k))
     # #  target course
     # state = State(x=-0.0, y=5.0, yaw=np.radians(20.0), v=0.0)
     # ax = [0.0, 100.0, 100.0, 50.0, 60.0]
@@ -195,16 +207,16 @@ def main():
     # ax = [0.0, 20.0]
     # ay = [0.0,  0.0]
 
-    # Lane Change Course
-    state = State(x=0.0, y=0.0, yaw=0.0, v=0.0)
-    ax = [0.0, 29.0, 30.0, 40.0, 41.0, 100.0]
-    ay = [0.0,  0.0,  0.0,  3.0,  3.0,   3.0]
+    # # Lane Change Course
+    # state = State(x=0.0, y=0.0, yaw=0.0, v=0.0)
+    # ax = [0.0, 29.0, 30.0, 40.0, 41.0, 100.0]
+    # ay = [0.0,  0.0,  0.0,  3.0,  3.0,   3.0]
 
-    # # Figure 8 Course
-    # state = State(x=0.0, y=1.0, yaw=0.0, v=0.0)
-    # r = 65.0
-    # ax = [0.0, r, 0.0, -r, 0.0,  r,  0.0, -r, 0.0]
-    # ay = [0.0, r, 2*r,  r, 0.0, -r, -2*r, -r, 0.0]
+    # Figure 8 Course
+    state = State(x=0.0, y=1.0, yaw=0.0, v=0.0)
+    r = 65.0
+    ax = [0.0, r, 0.0, -r, 0.0,  r,  0.0, -r, 0.0]
+    ay = [0.0, r, 2*r,  r, 0.0, -r, -2*r, -r, 0.0]
 
     # # Road Path 
     # state = State(x=0.0, y=1.0, yaw=0.0, v=0.0)
@@ -212,7 +224,7 @@ def main():
     # ay = [0.0, -50.0, -135.0, -110.0, -125.0, -35.0,  -35.0, -150.0, -190.0, -190.0, -190.0, -150.0,  60.0, 130.0, 150.0,  60.0, 110.0,  120.0,    0.0, 0.0]
 
     cx, cy, cyaw, ck, s = cubic_spline_planner.calc_spline_course(
-        ax, ay, ds=5)
+        ax, ay, ds=ds)
 
     # target_speed = 30.0 / 3.6  # [m/s]
     # target_speed = 5.0 # [m/s]
@@ -226,17 +238,18 @@ def main():
     yaw = [state.yaw]
     v = [state.v]
     t = [0.0]
-    # idx_now, _ = calc_target_index(state, cx, cy)
-    idx_now, _, _ = calc_next_index(state, cx, cy, 0) # start at index 0 
-
-    # print(cx[1])
     e_ct_arr = []
     station = []
     sta_cum = 0.
+    sta_before = 0.
+    idx_now = 0
+
+    # idx_now, _ = calc_target_index(state, cx, cy)
+    # idx_now, _, _, _ = calc_next_index(state, cx, cy, idx_now, sta_before) # start at index 0 
 
     while max_simulation_time >= time and idx_last > idx_now:
         ai = pid_control(target_speed, state.v)
-        di, idx_now, e_ct, sta = stanley_control(state, cx, cy, cyaw, idx_now)
+        di, idx_now, e_ct, sta, sta_before = stanley_control(state, cx, cy, cyaw, idx_now, sta_before)
         state.update(ai, di)
 
         time += dt
@@ -260,7 +273,7 @@ def main():
         #     plt.axis("equal")
         #     plt.grid(True)
         #     plt.title("Speed[km/h]:" + str(state.v * 3.6)[:4])
-        #     plt.pause(0.001)
+        #     plt.pause(0.0001)
 
     # Test
     assert idx_last >= idx_now, "Cannot reach goal"
@@ -277,32 +290,34 @@ def main():
         plt.plot(station, e_ct_arr, c=color, label="k={}".format(k))
         plt.xlabel("Station[m]")
         plt.ylabel("Cross-Track Error[m]")
-        plt.title("Station vs. Cross-Track Error")
+        plt.title("Station vs. Cross-Track Error at {} [m/s]".format(target_speed))
         plt.grid(True)
         plt.legend(loc="best")
 
-        # Plot course and car path
-        plt.figure()
-        plt.plot(cx, cy, "-r", label="course")
-        # plt.scatter(cx, cy, c="k", markersize=0.1)
-        plt.plot(x, y, "-b", label="trajectory")
-        plt.legend()
-        plt.xlabel("x[m]")
-        plt.ylabel("y[m]")
-        plt.title("Course with gain of k={}".format(k))
-        plt.axis("equal")
-        plt.grid(True)
+        # # Plot course and car path
+        # plt.figure()
+        # plt.plot(cx, cy, "-r", label="course")
+        # # plt.scatter(cx, cy, c="k", markersize=0.1)
+        # plt.plot(x, y, "-b", label="trajectory")
+        # plt.legend()
+        # plt.xlabel("x[m]")
+        # plt.ylabel("y[m]")
+        # plt.title("Course with gain of k={}".format(k))
+        # plt.axis("equal")
+        # plt.grid(True)
 
     return 0
 
 if __name__ == '__main__':
     Kp = 1.0  # speed proportional gain
-    dt = 0.1  # [s] time difference
+    factor = 10
+    dt = 0.1 / factor  # [s] time difference
     L = 2.9  # [m] Wheel base of vehicle
     max_steer = np.radians(30.0)  # [rad] max steering angle
     show_animation = True
+    ds = 1 # for the cublic spline planner
 
-    target_speed = 5.0 # [m/s]
+    target_speed = 10.0 # [m/s]
     
     k = 0.5  # control gain
     color = 'red'
