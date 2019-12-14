97d1f983588bc7ca1246810a987273157290e4cb
Justin Rooneygit config --global user.name Justin <justin@dexai.com>
2019-12-14 08:24:24 -0500

    docs: stanley simulations
    cleaned up code, made comments fmore readable

--- PathTracking/stanley_controller/stanley_controller.py
+++ PathTracking/stanley_controller/stanley_controller.py
@@ -2,7 +2,7 @@
 
 Path tracking simulation with Stanley steering control and PID speed control.
 
-author: Atsushi Sakai (@Atsushi_twi)
+author: Atsushi Sakai (@Atsushi_twi) with Improvements by Justin Rooney
 
 Ref:
     - [Stanley: The robot that won the DARPA grand challenge](http://isl.ecst.csuchico.edu/DOCS/darpa2005/DARPA%202005%20Stanley.pdf)
@@ -55,7 +55,6 @@ class State(object):
         self.yaw = normalize_angle(self.yaw)
         self.v += acceleration * dt
 
-
 def normalize(a):
     """
     Return the normal unit vector.
@@ -78,7 +77,6 @@ def pid_control(target, current):
     """
     return Kp * (target - current)
 
-
 def stanley_control(state, cx, cy, cyaw, idx_now, sta_before):
     """
     Stanley steering control.
@@ -90,11 +88,7 @@ def stanley_control(state, cx, cy, cyaw, idx_now, sta_before):
     :param idx_now: (int)
     :return: (float, int)
     """
-    # idx_next, e_ct = calc_target_index(state, cx, cy)
     idx_next, e_ct, sta, sta_before = calc_next_index(state, cx, cy, idx_now, sta_before)
-
-    # if idx_now >= idx_next:
-    #     idx_next = idx_now
 
     # theta_e corrects the heading error
     theta_e = normalize_angle(cyaw[idx_next] - state.yaw)
@@ -104,7 +98,6 @@ def stanley_control(state, cx, cy, cyaw, idx_now, sta_before):
     delta = theta_e + theta_d
 
     return delta, idx_next, e_ct, sta, sta_before
-
 
 def normalize_angle(angle):
     """
@@ -121,33 +114,6 @@ def normalize_angle(angle):
 
     return angle
 
-
-def calc_target_index(state, cx, cy):
-    """
-    Compute index in the trajectory list of the target.
-
-    :param state: (State object)
-    :param cx: [float]
-    :param cy: [float]
-    :return: (int, float)
-    """
-    # Calc front axle position
-    fx = state.x + L * np.cos(state.yaw)
-    fy = state.y + L * np.sin(state.yaw)
-
-    # Search nearest point index
-    dx = [fx - icx for icx in cx]
-    dy = [fy - icy for icy in cy]
-    d = np.hypot(dx, dy)
-    idx_now = np.argmin(d)
-
-    # Project RMS error onto front axle vector
-    front_axle_vec = [-np.cos(state.yaw + np.pi / 2),
-                      -np.sin(state.yaw + np.pi / 2)]
-    e_ct = np.dot([dx[idx_now], dy[idx_now]], front_axle_vec)
-
-    return idx_now, e_ct
-
 def calc_next_index(state, cx, cy, idx_now, sta_before):
     """
     Compute next index in the trajectory.
@@ -157,7 +123,7 @@ def calc_next_index(state, cx, cy, idx_now, sta_before):
     :param cy: [float]
     :return: next_idx, e_ct (cross-track error), sta (station) [int, float, float]
     """
-    # Calc front axle position
+    # Calculate front axle position and use for index incrementing
     fx = state.x + L * np.cos(state.yaw)
     fy = state.y + L * np.sin(state.yaw)
 
@@ -165,49 +131,45 @@ def calc_next_index(state, cx, cy, idx_now, sta_before):
     r_T = np.array([cx[idx_now], cy[idx_now]]) # desired position vector
     r = np.array([fx, fy])
 
-    sta_vect = np.array([cx[idx_next] - cx[idx_now], cy[idx_next] - cy[idx_now]]) # station vector, or vector from current point on path to next point on path
-    sta = np.linalg.norm(sta_vect) # magnitude of the station vector
-    a = sta_vect / np.linalg.norm(sta) # trajectory tangent unit vector
+    # Course vector, or vector along current segment of the course
+    course_vect = np.array([cx[idx_next] - cx[idx_now], cy[idx_next] - cy[idx_now]])
+    sta = np.linalg.norm(course_vect) # Magnitude of the course vector
+    t = course_vect / np.linalg.norm(sta) # Tangent unit vector
 
-    # sta_before = sta
-    # print('The station before is: {}'.format(sta_before))
-    sta = np.dot((r - r_T), a)
-    # print('The station is: {}'.format(sta))
-    sta_inc = sta - sta_before # station is actually the projection of the position onto the tangent vector minus the previous amount
+    # Calculate station - the projection of the vehicle position along the course segment
+    sta = np.dot((r - r_T), t)
+
+    # Calculate station increment - the amount of station increased per function call
+    sta_inc = sta - sta_before
     if sta_inc < 0:
         sta_inc += ds
-    # print('The station increment is: {}'.format(sta_inc))
+    
+    # Store the amount of station for use upon consequtive function call
     sta_before = sta
 
-    normal = normalize(a)
-    # print('The tangent vector is: {}'.format(a))
-    # print('The normal vector is: {}'.format(normal))
+    # Find the normal unit vector
+    n = normalize(t)
 
-    # Eq. 9 in the following paper:
+    # Find cross-track position error - Eq. 9 in the following paper:
     # http://ai.stanford.edu/~gabeh/papers/GNC08_QuadTraj.pdf
-    e_ct = np.dot((r_T - r), normal) # cross-track position error
+    e_ct = np.dot((r_T - r), n)
 
-    b = np.array([fx - cx[idx_next], fy - cy[idx_next]]) # vector from next index point to current robot state
-    # print('The next index to state vector is: {}'.format(b))
+    # Find vector from next index point to state of robot front axle
+    b = np.array([fx - cx[idx_next], fy - cy[idx_next]]) 
 
-    angle = np.arccos(np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b)))
+    # Find angle between robot state
+    angle = np.arccos(np.dot(t, b) / (np.linalg.norm(t) * np.linalg.norm(b)))
 
-    # print('The angle is: {}'.format(angle))
+    # The waypoint will increment once the vehicle passes the normal vector
     if angle < (np.pi/2):
-        # print("incremented the counter")
         idx_now += 1
 
-    # print('idx_next: {}'.format(idx_next))
-    # print('cross-track error: {}'.format(e_ct))
-
     return idx_now, e_ct, sta_inc, sta_before
-
 
 def getLaneChange():
     ax = [0.0, 29.0, 30.0, 40.0, 41.0, 100.0]
     ay = [0.0,  0.0,  0.0,  3.0,  3.0,   3.0]
     return ax, ay, 'Lane Change Course'
-
 
 def getFigureEight():
     r = 65.0
@@ -229,7 +191,7 @@ def main():
     print("Starting main loop with k={}".format(k))
     state = State(x=0.0, y=0.5, yaw=0.0, v=0.0)
 
-    # Chose path
+    ### PICK PATH HERE! ###
     ax, ay, course_type = getLaneChange()
     # ax, ay, course_type = getFigureEight()
     # ax, ay, course_type = getRoadPath()
@@ -268,27 +230,24 @@ def main():
         v.append(state.v)
         t.append(time)
 
-        # if show_animation:  # pragma: no cover
-        #     plt.cla()
-        #     plt.plot(cx, cy, ".r")
-        #     # plt.plot(cx, cy, ".r", label="course")
-        #     plt.plot(x, y, "-b")
-        #     # plt.plot(x, y, "-b", label="trajectory")
-        #     plt.plot(cx[idx_now], cy[idx_now], "xg", label="target")
-        #     plt.axis("equal")
-        #     plt.grid(True)
-        #     plt.title("Speed[km/h]:" + str(state.v * 3.6)[:4])
-        #     plt.pause(0.0001)
+        if show_animation:
+            plt.cla()
+            plt.plot(cx, cy, ".r")
+            plt.plot(x, y, "-b")
+            plt.plot(cx[idx_now], cy[idx_now], "xg", label="target")
+            plt.axis("equal")
+            plt.grid(True)
+            plt.title("Speed[km/h]:" + str(state.v * 3.6)[:4])
+            plt.pause(0.0001)
 
-    # Test
     assert idx_last >= idx_now, "Cannot reach goal"
 
-    if show_animation:  # pragma: no cover
+    if show_plots:
         # Plot course and car path
         plt.figure(1)
-        plt.plot(cx, cy, "-r")
-        # plt.scatter(cx, cy, c="k", markersize=0.1)
-        # plt.plot(x, y, "-b")
+        plt.plot(cx, cy, "-r", label='course')
+        plt.scatter(cx, cy, c="k", s=0.8)
+        plt.plot(x, y, "-b", label='trajectory')
         plt.legend()
         plt.xlabel("x[m]")
         plt.ylabel("y[m]")
@@ -296,29 +255,29 @@ def main():
         plt.axis("equal")
         plt.grid(True)
 
-        # Plot errors
+        # Plot relevant errors
         plt.figure(0)
         plt.plot(station, e_ct_arr, c=color, label="k={}".format(k))
         plt.xlabel("Station[m]")
         plt.ylabel("Cross-Track Error[m]")
         plt.title("Stanley at {}m/s on {}".format(target_speed, course_type))
         plt.grid(True)
-        # plt.legend(loc="best")
 
     return 0
 
 if __name__ == '__main__':
-    Kp = 1.0  # speed proportional gain
-    factor = 15
-    dt = 0.1 / factor  # [s] time difference
-    L = 2.9  # [m] Wheel base of vehicle
+    Kp = 1.0                      # speed proportional gain
+    dt = 0.01                     # [s] Time difference
+    L = 2.9                       # [m] Wheel base of vehicle
     max_steer = np.radians(30.0)  # [rad] max steering angle
-    show_animation = True
-    ds = 5 # for the cublic spline planner
+    show_animation = False        # Turn on animation
+    show_plots = True             # Turn on course and error plots
+    ds = 1                        # [m] Segment length for the cublic spline planner
 
-    target_speed = 20.0 # [m/s]
+    target_speed = 20.0           # [m/s] Speed reference
     
-    k = 1.0  # control gain
+    # Iterate through set of steering gains and plot cross-track error vs. station
+    k = 1.0
     color = 'black'
     main()
 
@@ -342,8 +301,9 @@ if __name__ == '__main__':
     color = 'red'
     main()
 
-    plt.legend(loc="best")
-    plt.savefig('images/stanley_lane_change_{}.png'.format(target_speed))
+    # Save out plots as png files
+    # plt.savefig('images/stanley_lane_change_{}.png'.format(target_speed))
     # plt.savefig('images/stanley_figure_eight_{}.png'.format(target_speed))
     # plt.savefig('images/stanley_road_{}.png'.format(target_speed))
+    plt.legend(loc="best")
     plt.show()
